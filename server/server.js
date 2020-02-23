902742ee9d9eb83279f0af6039ff89c1c2d046df
Lorman Lau <lormanlau@gmail.com>
2020-02-22 16:12:23 -0800

    removed default serving index.html

--- server/server.js
+++ server/server.js
@@ -4,7 +4,7 @@ const axios = require('axios');
 const bodyParser = require('body-parser');
 const app = express();
 
-app.use(express.static(path.join(__dirname, '../public')))
+app.use(express.static(path.join(__dirname, '../public'), {index: false}))
 
 app.listen("5000", function() { 
     return console.log("Server running on 5000")
