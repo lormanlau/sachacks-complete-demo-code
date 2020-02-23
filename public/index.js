c0cb35101df02ec62e531f7217648998b55a41f1
Lorman Lau <lormanlau@gmail.com>
2020-02-23 10:10:16 -0800

    finished demo code

--- public/index.js
+++ public/index.js
@@ -1,4 +1,28 @@
 var my_super_secret_api_key = "password"
-document.addEventListener("DOMContentLoaded", (event) => {
-    
+document.addEventListener("DOMContentLoaded", function(event) {
+    var input = document.getElementById('text')
+    var button = document.getElementById('button')
+    var main = document.getElementById('main')
+    var img = document.getElementById('pokemon')
+    console.log(input)
+    console.log(button)
+    button.addEventListener('click', function(event) {
+        fetch("/test/" + input.value)
+        .then(function(data) {
+            return data.json()
+        })
+        .then(function(json) {
+            img.setAttribute('src', json.results.sprites.front_default)
+        })
+
+        // fetch("/grabsomething", {
+        //     method: 'POST',
+        //     headers: { 'Content-Type': 'application/json' },
+        //     body: JSON.stringify({"input": input.value})
+        // }).then(function(response) {
+        //     return response.json();
+        // }).then(function(json) {
+        //     img.setAttribute('src', json.results.sprites.front_default)
+        // })
+    })
 });
--- server/server.js
+++ server/server.js
@@ -5,6 +5,32 @@ const bodyParser = require('body-parser');
 const app = express();
 
 app.use(express.static(path.join(__dirname, '../public'), {index: false}))
+app.use(bodyParser.urlencoded({extended: false}))
+app.use(bodyParser.json())
+
+app.get('/', function(request, response) {
+    console.log(__dirname)
+    response.sendFile(path.join(__dirname, "../public/index.html"));
+})
+
+app.get('/test/:pokemon', function(request, response) {
+    console.log(request.params.pokemon)
+    axios.get('https://pokeapi.co/api/v2/pokemon/' + request.params.pokemon)
+    .then(function (result) {
+        response.send({"results": result.data})
+    })
+    // response.send({"results": "Done"})
+})
+
+app.post('/grabsomething', function(req, res) {
+    console.log(req.body)
+    axios.get('https://pokeapi.co/api/v2/pokemon/' + req.body.input)
+    .then(function (result) {
+        res.send({"results": result.data})
+    })
+    // res.send({"results": "Done"})
+})
+
 
 app.listen("5000", function() { 
     console.log("Server running on 5000")
